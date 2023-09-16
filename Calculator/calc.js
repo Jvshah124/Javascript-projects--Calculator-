f7e5720bc464e3927672e2a53ce7710009d2de5e
Jv Shah <88554440+Jvshah124@users.noreply.git>
2023-09-16 11:22:20 +0530

    js calculator

---
+++ Calculator/calc.js
@@ -0,0 +1,29 @@
+// My first JS Project
+
+let string = "";
+let buttons = document.querySelectorAll(".button");
+// queryselectorAll will select all buttons with class button
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML == "C") {
+      string = "";
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML == "M+") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML == "%") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    } else {
+      console.log(e.target);
+      string = string + e.target.innerHTML;
+      document.querySelector("input").value = string;
+    }
+  });
+});
